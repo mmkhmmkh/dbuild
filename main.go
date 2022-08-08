abd5d0a9c98320f88dbfe4c780376edf8f550545
Mahdi Khancherli <vbha.mmk@gmail.com>
2022-08-08 15:37:15 +0430

    Added startController.

--- main.go
+++ main.go
@@ -5,10 +5,37 @@ import (
 	"github.com/mmkhmmkh/dbuild/pkg/hamctl"
 )
 
+const (
+	dbuildRepo        = "mmkhmmkh/dbuild"
+	controllerContext = "controller"
+	workerContext     = "worker"
+	dbuildBranch      = "master"
+	dbuildPrefix      = "dbuild-"
+)
+
+func StartController() error {
+	fmt.Printf("[ORCH] Starting controller...\n")
+	err := hamctl.CreateApp(dbuildPrefix+controllerContext, dbuildRepo, dbuildBranch, controllerContext)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[ORCH] Controller started successfuly.\n")
+
+	return nil
+}
+
 // main is entry for orchestrator node
 func main() {
-	err := hamctl.CreateApp("dbuild-worker-1", "mmkhmmkh/dbuild", "master")
+
+	fmt.Println("#########################")
+	fmt.Println("## dbuild Orchestrator ##")
+	fmt.Println("## By Mahdi Khancherli ##")
+	fmt.Println("#########################")
+
+	err := StartController()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("[ORCH] [ERROR] %v\n", err)
+		return
 	}
 }
--- pkg/hamctl/hamctl.go
+++ pkg/hamctl/hamctl.go
@@ -18,8 +18,99 @@ const (
 	organizationName = "vbhammk"
 )
 
+func RemoveApp(appName string) error {
+	var args []string
+	args = append(args, "apps", "del")
+	args = append(args, appName)
+	args = append(args, "-n", dbuildNamespace)
+	args = append(args, "-c", clusterName)
+	args = append(args, "-o", organizationName)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	cmd := exec.Command(hamctlBinPath, args...)
+
+	fmt.Println("Running: ", hamctlBinPath, strings.Join(args, " "))
+
+	stdoutIn, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("failed getting hamctl output with args (%s) with error %s", args, err)
+	}
+
+	stdinOut, err := cmd.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("failed getting hamctl input with args (%s) with error %s", args, err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("failed starting hamctl with args (%s) with error %s", args, err)
+	}
+
+	go func() {
+		for sig := range c {
+			fmt.Println("got signal ", sig)
+
+			if err := cmd.Process.Kill(); err != nil {
+				fmt.Printf("failed to kill process: %v", err)
+			}
+		}
+	}()
+
+	defer stdoutIn.Close()
+	defer stdinOut.Close()
+
+	split := func(data []byte, atEOF bool) (advance int, token []byte, spliterror error) {
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+		if i := bytes.IndexByte(data, '\n'); i >= 0 {
+			// We have a full newline-terminated line.
+			return i + 1, data[0:i], nil
+		}
+		if i := bytes.IndexByte(data, '\r'); i >= 0 {
+			// We have a cr terminated line
+			return i + 1, data[0:i], nil
+		}
+		if atEOF {
+			return len(data), data, nil
+		}
+
+		return 0, nil, nil
+	}
+
+	scanner := bufio.NewScanner(stdoutIn)
+
+	scanner.Split(split)
+
+	buf := make([]byte, 2)
+	scanner.Buffer(buf, bufio.MaxScanTokenSize)
+	//fmt.Println("BEGINS")
+	for scanner.Scan() {
+		line := scanner.Text()
+		//fmt.Println(line)
+		if strings.Contains(line, "Enter app name to confirm") {
+			io.WriteString(stdinOut, fmt.Sprintf("%s\r", appName))
+			break
+		}
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		//fmt.Println(line)
+		if strings.Contains(line, "✗") && !strings.Contains(line, "Enter app name to confirm") {
+			return fmt.Errorf("hamctl error: %s", strings.TrimSpace(strings.ReplaceAll(line, "✗ ", "")))
+		} else if strings.Contains(line, "deleted successfully") {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("hamctl unexpected behaviour")
+}
+
 // CreateApp creates new app on Hamravesh PaaS
-func CreateApp(appName string, repoName string, branchName string) error {
+func CreateApp(appName string, repoName string, branchName string, buildContext string) error {
 	var args []string
 	args = append(args, "apps", "create")
 	args = append(args, "-n", appName)
@@ -33,7 +124,7 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	cmd := exec.Command(hamctlBinPath, args...)
 
-	fmt.Println(hamctlBinPath, strings.Join(args, " "))
+	fmt.Println("Running: ", hamctlBinPath, strings.Join(args, " "))
 
 	stdoutIn, err := cmd.StdoutPipe()
 	if err != nil {
@@ -93,30 +184,31 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	buf := make([]byte, 2)
 	scanner.Buffer(buf, bufio.MaxScanTokenSize)
-	fmt.Println("BEGINS")
+	//fmt.Println("BEGINS")
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, fmt.Sprintf("➤ %s", repoName)) {
 			io.WriteString(stdinOut, "\r")
 			break
-		} else if strings.Contains(line, repoName) {
+		} else if strings.Contains(line, repoName) || strings.Contains(line, "➤ ") {
+			//fmt.Println("HERE")
 			io.WriteString(stdinOut, "\x1B[B")
 		}
 		//time.Sleep(100 * time.Millisecond)
 	}
 
-	fmt.Println("ENDS")
+	//fmt.Println("ENDS")
 
 	//time.Sleep(500 * time.Millisecond)
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, fmt.Sprintf("➤ %s", branchName)) {
 			io.WriteString(stdinOut, "\r")
 			break
-		} else if strings.Contains(line, branchName) {
+		} else if strings.Contains(line, branchName) || strings.Contains(line, "➤ ") {
 			io.WriteString(stdinOut, "\x1B[B")
 		}
 		//time.Sleep(100 * time.Millisecond)
@@ -124,25 +216,25 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, "Dockerfile address") {
-			io.WriteString(stdinOut, ".\r")
+			io.WriteString(stdinOut, fmt.Sprintf("./%s/.\r", buildContext))
 			break
 		}
 	}
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, "Build Context") {
-			io.WriteString(stdinOut, ".\r")
+			io.WriteString(stdinOut, fmt.Sprintf("./%s/.\r", buildContext))
 			break
 		}
 	}
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, "Auto deploy after push") {
 			io.WriteString(stdinOut, "no\r")
 			break
@@ -151,7 +243,7 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, "Service port") {
 			io.WriteString(stdinOut, "\r")
 			break
@@ -160,8 +252,8 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
-		if strings.Contains(line, "Runnng command of your project") {
+		//fmt.Println(line)
+		if strings.Contains(line, "command of your project") {
 			io.WriteString(stdinOut, "\r")
 			break
 		}
@@ -169,7 +261,7 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, "Args of running command") {
 			io.WriteString(stdinOut, "\r")
 			break
@@ -178,7 +270,7 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if strings.Contains(line, "Health Check Path") {
 			io.WriteString(stdinOut, "\r")
 			break
@@ -187,8 +279,21 @@ func CreateApp(appName string, repoName string, branchName string) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
+		if strings.Contains(line, "Plan 1") {
+			io.WriteString(stdinOut, "\r")
+			break
+		}
 	}
 
-	return nil
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "✗") {
+			return fmt.Errorf("hamctl error: %s", strings.TrimSpace(strings.ReplaceAll(line, "✗ ", "")))
+		} else if strings.Contains(line, "created successfully") {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("hamctl unexpected behaviour")
 }
